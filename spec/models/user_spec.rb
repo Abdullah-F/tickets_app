f6b9b01d7fbf77792860ad9a16d509d379098ddd
Abdullah Fadhel <abdullahfadhel@outlook.com>
2021-03-06 01:18:48 +0200

    fix: update linting for user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  describe '.send_due_date_reminders' do
+  describe ".send_due_date_reminders" do
     let(:send_due_date_reminders) do
       {
         on_due_date: 0,
-        one_day_before: 1,
+        one_day_before: 1
       }
     end
-    it 'maps keys to values correctly' do
+    it "maps keys to values correctly" do
       expect(send_due_date_reminders)
         .to eq(described_class.send_due_date_reminders.symbolize_keys)
     end
