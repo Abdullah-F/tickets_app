59887625ca453ff2532fc02196846211313d5e17
Abdullah Fadhel <abdullahfadhel@outlook.com>
2021-03-06 01:14:10 +0200

    feat: add send_due_date_remainder enum to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  enum send_due_date_reminder: {on_due_date: 0, one_day_before: 1}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.send_due_date_reminders' do
+    let(:send_due_date_reminders) do
+      {
+        on_due_date: 0,
+        one_day_before: 1,
+      }
+    end
+    it 'maps keys to values correctly' do
+      expect(send_due_date_reminders)
+        .to eq(described_class.send_due_date_reminders.symbolize_keys)
+    end
+  end
 end
