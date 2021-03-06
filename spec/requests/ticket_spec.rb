6a19344dcd0bd56fc7afe846694a2ded0a31b032
Abdullah Fadhel <abdullahfadhel@outlook.com>
2021-03-06 09:44:19 +0200

    fix: run linting

--- spec/requests/ticket_spec.rb
+++ spec/requests/ticket_spec.rb
@@ -1,29 +1,29 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'tickets', type: :request do
-  describe 'POST tickets#create' do
-    context 'when all params are valid' do
+RSpec.describe "tickets", type: :request do
+  describe "POST tickets#create" do
+    context "when all params are valid" do
       let(:user) do
         create(:user)
       end
 
       let(:ticket_params) do
-        { ticket: attributes_for(:ticket, assigned_user_id: user.id) }
+        {ticket: attributes_for(:ticket, assigned_user_id: user.id)}
       end
 
-      it 'creates a ticket' do
-        post '/tickets', :params => ticket_params
+      it "creates a ticket" do
+        post "/tickets", params: ticket_params
         expect(response).to have_http_status(:created)
       end
     end
 
-    context 'when params are invalid' do
+    context "when params are invalid" do
       let(:ticket_params) do
-        { ticket: attributes_for(:ticket, assigned_user_id: nil) }
+        {ticket: attributes_for(:ticket, assigned_user_id: nil)}
       end
 
-      it 'fails with :unprocessable_entity response code' do
-        post '/tickets', :params => ticket_params
+      it "fails with :unprocessable_entity response code" do
+        post "/tickets", params: ticket_params
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
