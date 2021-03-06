54a1286bc04349f6e24bb626915f2b53c4f93a5f
Abdullah Fadhel <abdullahfadhel@outlook.com>
2021-03-06 19:00:37 +0200

    feat: add separate container for cron_jobs

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,14 +1,4 @@
 class Ticket < ApplicationRecord
-  class<<self
-    private
-
-    def self.due_time_db_format(due_time)
-      due_time
-        .change({day: 1, month: 1, year: 2000})
-        .beginning_of_minute.to_s(:db)
-    end
-  end
-
   belongs_to :assigned_user, class_name: User.name
   validates_presence_of :title, :description, :due_date
 
@@ -16,4 +6,10 @@ class Ticket < ApplicationRecord
   scope :by_assigned_user_due_time, ->(due_time) {
     where("users.due_date_reminder_time = '#{due_time_db_format(due_time)}'")
   }
+
+  def self.due_time_db_format(due_time)
+    due_time
+      .change({day: 1, month: 1, year: 2000})
+      .beginning_of_minute.to_s(:db)
+  end
 end
